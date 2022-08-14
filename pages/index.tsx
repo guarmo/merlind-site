e59e894308d648bd64903f8357323018dd7398dc
Armando Guarino <armando.guarino@berlin-bytes.de>
2022-08-14 18:14:47 +0200

    Adjust arrangement

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,15 +10,16 @@ import { BsTelephone } from "react-icons/bs";
 
 const Home: NextPage = () => {
   return (
-    <div className="w-screen h-screen bg-gray-500 overflow-hidden">
+    <div className="w-screen h-screen bg-gray-500 sm:overflow-hidden">
       <Meta />
       <main className="flex flex-col md:flex-row w-full h-full">
-        <section className="main-img w-full md:w-1/2 self-center relative">
+        <section className="main-img w-full md:w-1/2 h-1/2 self-center relative">
           <h1 className="whitespace-nowrap absolute bottom-0 left-0 text-white pl-2 pb-2 text-xs hidden sm:block">
             photo by Vivien Woßeng and Enno Schramm
           </h1>
         </section>
-        <section className="self-center flex flex-col items-center justify-center w-full md:w-1/2 h-4/5">
+        {/* h-1/2 md:h-4/5 */}
+        <section className="self-center flex flex-col items-center justify-center w-full md:w-1/2 h-1/2 md:h-4/5">
           <div className="text-center mb-6 md:mb-20 text-white sm:text-xl xl:text-3xl font-semibold">
             <h1 className="font-montserrat">MERLIND CONSTANZE POHL</h1>
             <h1 className="tracking-widest my-2 sm:my-5">— mezzosoprano —</h1>
@@ -63,7 +64,7 @@ const Home: NextPage = () => {
               <h1>merlindconstanze</h1>
             </a>
           </div>
-          <h1 className="w-full md:w-1/2 text-center md:text-right md:w-1/2 text-white text-xs absolute bottom-0 right-0 pb-2 sm:pr-2">
+          <h1 className="w-full text-center md:text-right md:w-1/2 text-white text-xs absolute bottom-0 right-0 pb-2 sm:pr-2">
             ©2022 Merlind Constanze Pohl
           </h1>
         </section>
